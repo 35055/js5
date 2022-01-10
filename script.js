421ce7b9a572b88c2bb62a532c0567ad96d3eff1
Murat <bxndbdbvsj@gmail.com>
2022-01-10 10:37:14 +0300

    end

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// for(let i = 1; i <= 10; i++){
+//     let name = prompt('Введите имя');
+//     let age  = +prompt('Введите возраст');
+//     let con1 = (`Пользователь ${i} ${name}`);
+//     let con2 = (` Возраст ${age}`);
+//     let maincon = con1 + con2;
+//     console.log(maincon); 
+// } первый способ
+
+
+
+
+
+
 for(let i = 1; i <= 10; i++){
-    let name = prompt('Введите имя');
-    let age  = +prompt('Введите возраст');
-    let con1 = (`Пользователь ${i} ${name}`);
-    let con2 = (` Возраст ${age}`);
-    let maincon = con1 + con2;
-    console.log(maincon); 
+let nam = prompt('Введите имя');
+let num = +prompt('Введите возрвст');
+
+    let simple = {
+        dsd:(`Пользователь ${i}`),
+        name: (`Имя ${nam}`)  ,
+        age:(`Возраст ${num}`)
+    }
+
+    for(let key in simple){
+        console.log(simple[key]);  
+    }
+
 }
